ddf252946f332e2afd750002faed9520f688b187
Alex Manzi <alex.manzi22@gmail.com>
2022-01-18 17:05:28 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let element = document.querySelector('main');
+element.remove('main');
+
+
+let newHeader = document.createElement('H1');
+newHeader.id ='victory';
+newHeader.innerHTML = "Alex is the champion"
+
